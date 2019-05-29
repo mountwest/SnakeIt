51aaf5139301fdb5e2516669280f270690773cd0
Damian Wozniak <damian.pwozniak@gmail.com>
2019-05-29 13:59:13 +0200

    Added another label to explain further detail about how to play

--- Snake It AR/Snake It AR/HelpViewController.swift
+++ Snake It AR/Snake It AR/HelpViewController.swift
@@ -13,6 +13,7 @@ class HelpViewController: UIViewController {
     // Create a new label.
     var lblTitle = UILabel()
     var lblSection1 = UILabel()
+    var lblSection2 = UILabel()
 
     @IBOutlet weak var backButtonInNavBar: UIBarButtonItem!
     override func viewDidLoad() {
@@ -36,18 +37,28 @@ class HelpViewController: UIViewController {
         self.view.addSubview(lblTitle)
         
         lblSection1.frame = CGRect(x: 50, y: 150, width: 300, height: 200)
-        lblSection1.text = "To play this game, first press on your screen to load in the map and then spawn the snake."
+        lblSection1.text = "To play this game, first press on your screen to load in the map and then spawn the snake. After the snake spawns, it will begin to move. You can control the direction, by swiping left or right."
         lblSection1.textAlignment = .left
         lblSection1.textColor = UIColor.black
         lblSection1.font = UIFont(name: "Arial", size: 16)
         lblSection1.adjustsFontSizeToFitWidth = true
         lblSection1.minimumScaleFactor = 0.5
         lblSection1.allowsDefaultTighteningForTruncation = true
-        // I have to figure out line breaks.
-        lblSection1.numberOfLines = 10
+        lblSection1.numberOfLines = 20
         lblSection1.lineBreakMode = NSLineBreakMode.byWordWrapping
         
         self.view.addSubview(lblSection1)
+        
+        lblSection2.frame = CGRect(x: 50, y: 300, width: 300, height: 200)
+        lblSection2.text = "Collect fruits to collect points. Try to avoid the snake and walls! Hitting anything, that is NOT a fruit, will end the game!"
+        lblSection2.textAlignment = .left
+        lblSection2.textColor = UIColor.black
+        lblSection2.font = UIFont(name: "Arial", size: 16)
+        lblSection2.adjustsFontSizeToFitWidth = true
+        lblSection2.minimumScaleFactor = 0.5
+        lblSection2.allowsDefaultTighteningForTruncation = true
+        lblSection2.numberOfLines = 20
+        lblSection2.lineBreakMode = NSLineBreakMode.byWordWrapping
 
         // Do any additional setup after loading the view.
     }
