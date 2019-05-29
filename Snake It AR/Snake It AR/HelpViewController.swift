f3717dfc8b7bf8ca3600d8f37f3ee7df4cde2664
Damian Wozniak <damian.pwozniak@gmail.com>
2019-05-29 11:41:31 +0200

    Added labels in helpscreen, now to stylise

--- Snake It AR/Snake It AR/HelpViewController.swift
+++ Snake It AR/Snake It AR/HelpViewController.swift
@@ -9,10 +9,43 @@
 import UIKit
 
 class HelpViewController: UIViewController {
+    
+    // Create a new label.
+    var lblTitle = UILabel()
+    var lblSection1 = UILabel()
 
     @IBOutlet weak var backButtonInNavBar: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Create the title of the help.
+        // This creates the frame for the label, position and so on.
+        lblTitle.frame = CGRect(x: 0, y: 89, width: self.view.frame.width, height: 120)
+        // Add some text.
+        lblTitle.text = "HELP"
+        // Alignment.
+        lblTitle.textAlignment = .center
+        // Color of the background.
+        lblTitle.backgroundColor = UIColor.gray
+        // Color of the text.
+        lblTitle.textColor = UIColor.white
+        // The font.
+        lblTitle.font = UIFont(name: "Copperplate-bold", size: 22)
+        
+        // Add it to the view.
+        self.view.addSubview(lblTitle)
+        
+        lblSection1.frame = CGRect(x: 0, y: 240, width: self.view.frame.width, height: 120)
+        lblSection1.text = "To play this game, first press on your screen to load in the map and then spawn the snake."
+        lblSection1.textAlignment = .left
+        lblSection1.textColor = UIColor.black
+        lblSection1.font = UIFont(name: "Copperplate-bold", size: 16)
+        lblSection1.adjustsFontSizeToFitWidth = true
+        lblSection1.minimumScaleFactor = 0.5
+        lblSection1.allowsDefaultTighteningForTruncation = true
+        lblSection1.lineBreakMode = true
+        
+        self.view.addSubview(lblSection1)
 
         // Do any additional setup after loading the view.
     }
